f1f73c23752829f5efa29f7965e3b63b687104c7
SalomeKer <101712592+SalomeKer@users.noreply.github.com>
2022-07-14 16:51:01 +0200

    modification sauces

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,29 +1,26 @@
 const mongoose = require ("mongoose")
 const { unlink } = require("fs/promises")
-const res = require("express/lib/response")
-
 
 const productShema = new mongoose.Schema({
-userId: String,
-name : String,
-manufacturer : String,
-description : String,
-mainPepper : String,
-imageUrl : String,
-heat : Number,
-likes : Number,
-dislikes : Number,
-usersLiked : [String],
-usersDisliked: [String]
+  userId: String,
+  name : String,
+  manufacturer : String,
+  description : String,
+  mainPepper : String,
+  imageUrl : String,
+  heat : Number,
+  likes : Number,
+  dislikes : Number,
+  usersLiked : [String],
+  usersDisliked: [String]
 })
-
 const Product = mongoose.model("product", productShema)
 
 //Si l'utilisateur est authentifié on enchaine sur cette fonction
-function getSauces(req, res){
-         Product.find({})
-         .then(products => res.send(products))
-         .catch(error => res.status(500).send(error))
+function getSauces(req, res) {
+  Product.find({})
+    .then((products) => res.send(products))
+    .catch((error) => res.status(500).send(error))
 }
 
 function getSauce (req, res){
@@ -41,7 +38,6 @@ function getSaucesById(req, res){
 
 function deleteSauces (req, res){
   const { id } = req.params
-  //1 l'odre de suppression est envoyé à mongo 
   Product.findByIdAndDelete(id)
    .then((product) => sendClientResponse(product, res))
    .then ((item) => deleteImage(item))
@@ -53,11 +49,10 @@ function modifySauces(req, res){
     const {
       params: {id}
     } = req
-    
-    console.log("req.file", req.file)
-    
-    //Une nouvelle image a-t-elle était ajoutée ?
+        
+    //Une nouvelle image a-t-elle était ajoutée ? Si il y a une image, hasNewImage est sera égale a TRUE.
     const hasNewImage = req.file != null
+
     const payload = makePayload(hasNewImage, req)
 
     
@@ -78,10 +73,12 @@ function deleteImage(product){
 }
 
 function makePayload(hasNewImage, req){
-    //S'il n'y a pas d'image on renvoie le body tel quel 
     console.log("hasNewImage:", hasNewImage)
+    //S'il n'y a pas de nouvelle image (!) on renvoie le body tel quel
     if (!hasNewImage) return req.body
+    //Payload correspond à la nouvelle image à gerer
     const payload = JSON.parse(req.body.sauce)
+    //On récupère la bonne url
     payload.imageUrl = makeImageUrl(req, req.file.fileName)
     console.log("NOUVELLE IMAGE A GERER")
     console.log("Voici le payload:", payload)
@@ -89,69 +86,64 @@ function makePayload(hasNewImage, req){
 }
 
 function sendClientResponse(product,res){
-        if (product == null){
-            console.log("Les informations sont déja à jour")
-            return res.status(404).send({message: "Les informations sont déja à jour" })
-         }
-            console.log("ALL GOOD:", product)
-            return Promise.resolve(res.status(200).send(product)).then(()=> product)
-        }
-
-        
-        
-function makeImageUrl(req, fileName){
-    return req.protocol + "://" + req.get("host") + "/images/" + fileName
-    }
+   if (product == null){
+    console.log("Les informations sont déja à jour")
+    return res.status(404).send({ message: "Les informations sont déja à jour" })
+  }
+  console.log("ALL GOOD:", product)
+  return Promise.resolve(res.status(200).send(product)).then(() => product) //Le produit trouvé dans la base de donnée est renvoyé
+}
+//makeImageUrl l'url valide de l'image
+  function makeImageUrl(req, fileName){
+  return req.protocol + "://" + req.get("host") + "/images/" + fileName
+}
 
 
 
   function createSauces(req, res){
-   const body = req.body
-   const file = req.file
-   console.log({file})
-   const {fileName} = file
-
+   const { body, file } = req
+   const { fileName } = file
    const sauce = JSON.parse(body.sauce)
-
    const {name, manufacturer, description, mainPepper, heat, userId} = sauce
 
-   
-
-   const product = new Product({ 
-    userId,
-    name,
-    manufacturer,
-    description,
-    mainPepper,
+  const product = new Product({ 
+    userId: userId,
+    name: name,
+    manufacturer: manufacturer,
+    description: description,
+    mainPepper: mainPepper,
     imageUrl: makeImageUrl(req, fileName),
-    heat,
-    likes : 0,
-    dislikes : 0,
-    usersLiked : [],
+    heat: heat,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
     usersDisliked: []
  })
- product.save()
- .then((message) => {
-  res.status(201).send({message: message})
-  return console.log("produit enregistré", message)
- })
-   .catch(console.error)
-
+ product
+  .save()
+  .then((message) => res.status(201).send({ message }))
+  .catch((err) => res.status(500).send(err))
 }
+
+///LIKE SAUCE /////
 
 function likeSauce(req, res){
 const {like, userId} = req.body
-
-if(![1, -1, 0]. includes(like))return res.status(403).send({message: "invalid like value"})
+//ON vérifie si la valeur du like est bien égale a 1, -1 ou 0 dans le cas contraire (!=) on stop
+ if(![1, -1, 0].includes(like))return res.status(403).send({message: "invalid like value"})
+//Aray include permet de verifier les elements inclus
 
 
 getSauce(req, res)
+ //On récupère les information (like/dislike et utilisateur)
  .then((product) => updateVote(product, like, userId))
  .then(pr => pr.save())
  .then(prod => sendClientResponse (prod,res))
  .catch((err) => res.status(500).send(err))
+ //On gère le cas d'erreur 
 }
 
+//ON update les likes/dislike
 function updateVote(product, like, userId, res){
    if (like === 1  || like === -1 ) return incrementVote(product, userId, like)
    return resetVote(product, userId, res)
@@ -178,8 +170,9 @@ function resetVote(product, userId, res){
 
 function incrementVote(product, userId, like){
   let { usersLiked, usersDisliked } = product
-
+  //On définit l'array ciblé, Si like est égale à 1 c'est qu'on cherche a pusher dans usersLikes sinon c'est qu"on cherche a pusher dans usersDisliked (technique = ternary operator, permet de verifier selon une condition)
   const voterArray = like === 1 ? usersLiked : usersDisliked
+  //Si l'id est déja present dans l'array on n'update rien , dans le cas contraire on push
   if (voterArray.includes(userId))return product
   voterArray.push(userId)
 
